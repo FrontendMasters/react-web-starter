18905b8c7146cbcb254932ba438a23973018efac
Hendrixer <willscottmoss@gmail.com>
2016-06-30 09:39:54 -0500

    chore

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+      newTodo: ''
+    }
+  }
+  handleClick(e) {
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({todos, newTodo: ''});
+  }
+  
+  handleChange(e) {
+    this.setState({newTodo: e.target.value})
+  }
+  
   render() {
-    return (
-      <div>hello</div>
+    return(
+      <div>
+        <input type="text" value={this.state.newTodo} onChange={this.handleChange.bind(this)}/>
+        <button onClick={this.handleClick.bind(this)}>click</button>
+        {this.state.todos.map(todo => (<div>{todo}</div>))}
+      </div>
     );
   }
 }
