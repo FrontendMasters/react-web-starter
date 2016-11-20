49fa5565fa7cc522de345cbc034080ca496bc712
Bryan Gergen <bryangergen@gmail.com>
2016-11-20 17:19:02 -0500

    approach 2

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -4,27 +4,26 @@ export class Todo extends Component {
   constructor () {
     super();
     this.state = {
-      todos: [],
-      newTodo: ''
+      todos: []
     }
-  }
-
-  handleChange (e) {
-    const { value } = e.target;
-    this.setState({newTodo: value});
   }
 
   handleClick (e) {
     e.preventDefault();
-    const todos = [...this.state.todos, this.state.newTodo];
-    this.setState({todos, newTodo: ''});
+    const todos = [...this.state.todos, this.input.value];
+    this.input.value = '';
+    this.setState({todos});
   }
 
   render () {
     return (
       <div>
-        <input type="text" placeholder="new todo" value={this.state.newTodo} onChange={this.handleChange.bind(this)} />
-        <button onClick={this.handleClick.bind(this)}>Add Todo</button>
+        <form>
+          <input 
+            ref={node => this.input = node} 
+            type="text" placeholder="new todo" />
+          <button onClick={this.handleClick.bind(this)}>Add Todo</button>
+        </form>
         <ul>
           {this.state.todos.map(todo => <li>{todo}</li>)}
         </ul>
