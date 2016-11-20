270da09465a7530a73b945574c56af1c308be658
Bryan Gergen <bryangergen@gmail.com>
2016-11-20 16:42:17 -0500

    can add items to to-do list

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
-  render() {
+  constructor () {
+    super();
+    this.state = {
+      tasks: [],
+      value: ''
+    }
+  }
+
+  handleChange (e) {
+    this.setState({value: e.target.value});
+  }
+
+  handleClick () {
+    const tasks = [...this.state.tasks, this.state.value];
+    this.setState({tasks});
+  }
+
+  render () {
     return (
-      <div>hello</div>
+      <div>
+        <input type="text" value={this.state.value} onChange={this.handleChange.bind(this)} />
+        <button onClick={this.handleClick.bind(this)}>Add Task</button>
+        <ul>
+          {this.state.tasks.map(task => (<li key={task}>{task}</li>))}
+        </ul>
+      </div>
     );
   }
 }
