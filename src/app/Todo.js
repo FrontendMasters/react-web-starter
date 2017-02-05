4504f927d737eae819a100b71d6b0bab63220e02
[01;31m[K"name"[m[K: "Felix Vandelanotte <[01;31m[K"email"[m[K: "vandelanottefelix@gmail.com>
2017-02-05 18:48:10 +0100

    create react todo app

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,36 @@
 import React, { Component } from 'react';
 
+
 export class Todo extends Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: []
+    }
+  }
+  handleClick(e) {
+    e.preventDefault();
+    const todos = [...this.state.todos, this.input.value]
+    this.input.value = ""
+    this.setState({todos})
+  }
+  removeTodo(i) {
+    const todos = [...this.state.todos.slice(0,i), this.state.todos.slice(i + 1)]
+    this.setState({todos})
+  }
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <form>
+          <input ref={node => this.input = node} type="text" placeholder="new todo"></input>
+          <button onClick={this.handleClick.bind(this)}>create</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo, i) => (
+              <li onClick={() => this.removeTodo.call(this, i)}>{todo}</li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
